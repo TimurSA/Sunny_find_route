3eafc37ad450a7b20672d416c8ddbc69428da3b7
TimurSA <sayranov.timur@gmail.com>
2023-07-16 01:52:17 +0500

    добавил авторизацию + завершил приложение routes

--- routes/forms.py
+++ routes/forms.py
@@ -1,24 +1,51 @@
 from django import forms
-
 from cities.models import City
 from routes.models import Route
+from trains.models import Train
 
 
 class RouteForm(forms.Form):
-    from_city = forms.ModelChoiceField(label='Откуда', queryset=City.objects.all(),
-                                       widget=forms.Select(attrs={
-                                           'class': 'form-control js-example-basic-single'}))
-    to_city = forms.ModelChoiceField(label='Куда', queryset=City.objects.all(),
-                                     widget=forms.Select(attrs={
-                                         'class': 'form-control js-example-basic-single'}))
+    from_city = forms.ModelChoiceField(
+        label='Откуда', queryset=City.objects.all(), widget=forms.Select(
+            attrs={'class': 'form-control js-example-basic-single'}
+        )
+    )
+    to_city = forms.ModelChoiceField(
+        label='Куда', queryset=City.objects.all(), widget=forms.Select(
+            attrs={'class': 'form-control js-example-basic-single'}
+        )
+    )
     cities = forms.ModelMultipleChoiceField(
-        label='Через города', queryset=City.objects.all(), required=False, widget=forms.SelectMultiple(attrs={
-            'class': 'form-control js-example-basic-multiple'
+        label='Через города', queryset=City.objects.all(),
+        required=False, widget=forms.SelectMultiple(
+            attrs={'class': 'form-control js-example-basic-multiple'}
+        )
+    )
+    travelling_time = forms.IntegerField(
+        label='Время в пути', widget=forms.NumberInput(attrs={
+            'class': 'form-control', 'placeholder': 'Время в пути'})
+    )
+
+
+class RouteModelForm(forms.ModelForm):
+    name = forms.CharField(
+        label='Название маршрута', widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите название маршрута'
         }))
-    travelling_time = forms.IntegerField(label='Время в пути', widget=forms.NumberInput(attrs={
-        'class': 'form-control',
-        'placeholder': 'Время в пути'
-    }))
+    from_city = forms.ModelChoiceField(
+        queryset=City.objects.all(), widget=forms.HiddenInput()
+    )
+    to_city = forms.ModelChoiceField(
+        queryset=City.objects.all(), widget=forms.HiddenInput()
+    )
+    trains = forms.ModelMultipleChoiceField(
+        queryset=Train.objects.all(),
+        required=False, widget=forms.SelectMultiple(
+            attrs={'class': 'form-control d-none'}
+        )
+    )
+    travel_times = forms.IntegerField(widget=forms.HiddenInput())
 
     class Meta:
         model = Route
--- routes/views.py
+++ routes/views.py
@@ -1,10 +1,97 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, DeleteView
 
-# Create your views here.
+from cities.models import City
+from routes.forms import RouteForm, RouteModelForm
+from routes.models import Route
+from routes.utils import get_routes
+from trains.models import Train
 
-from routes.forms import RouteForm
 
-
+# @login_required
 def home(request):
     form = RouteForm()
     return render(request, 'routes/home.html', {'form': form})
+
+
+def find_routes(request):
+    if request.method == "POST":
+        form = RouteForm(request.POST)
+        if form.is_valid():
+            try:
+                context = get_routes(request, form)
+            except ValueError as e:
+                messages.error(request, e)
+                return render(request, 'routes/home.html', {'form': form})
+            return render(request, 'routes/home.html', context)
+        return render(request, 'routes/home.html', {'form': form})
+    else:
+        form = RouteForm()
+        messages.error(request, "Нет данных для поиска")
+        return render(request, 'routes/home.html', {'form': form})
+
+
+def add_route(request):
+    if request.method == "POST":
+        context = {}
+        data = request.POST
+        if data:
+            total_time = int(data['total_time'])
+            from_city_id = int(data['from_city'])
+            to_city_id = int(data['to_city'])
+            trains = data['trains'].split(',')
+            trains_lst = [int(t) for t in trains if t.isdigit()]
+            qs = Train.objects.filter(id__in=trains_lst).select_related(
+                'from_city', 'to_city')
+            cities = City.objects.filter(
+                id__in=[from_city_id, to_city_id]).in_bulk()
+            form = RouteModelForm(
+                initial={
+                    'from_city': cities[from_city_id],
+                    'to_city': cities[to_city_id],
+                    'travel_times': total_time,
+                    'trains': qs
+                         }
+            )
+            context['form'] = form
+        return render(request, 'routes/create.html', context)
+    else:
+        messages.error(request, "Невозможно сохранить несуществующий маршрут")
+        return redirect('/')
+
+
+def save_route(request):
+    if request.method == "POST":
+        form = RouteModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Маршрут успешно сохранен")
+            return redirect('/')
+        return render(request, 'routes/create.html', {'form': form})
+    else:
+        messages.error(request, "Невозможно сохранить несуществующий маршрут")
+        return redirect('/')
+
+
+class RouteListView(ListView):
+    paginate_by = 10
+    model = Route
+    template_name = 'routes/list.html'
+
+
+class RouteDetailView(DetailView):
+    queryset = Route.objects.all()
+    template_name = 'routes/detail.html'
+
+
+class RouteDeleteView(LoginRequiredMixin, DeleteView):
+    model = Route
+    success_url = reverse_lazy('home')
+
+    def get(self, request, *args, **kwargs):
+        messages.success(request, 'Маршрут успешно удален')
+        return self.post(request, *args, **kwargs)
